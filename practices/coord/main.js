59b8c5f17044693a7ab9f6d6bb8202b5d99b1c79
野民 <yemin.lxl@alibaba-inc.com>
2022-06-09 12:46:12 +0800

    fix: update

--- practices/coord/main.js
+++ practices/coord/main.js
@@ -2,7 +2,6 @@ import { initProgram } from "./program.js";
 import { draw } from './draw.js';
 import { initBuffers as initConeBuffer } from '../cone/buffer.js'
 import { initBuffer as initCylinderBuffer } from '../cylinder/buffer.js'
-import { createTool } from "../../lib/utils.js";
 import { create, perspective, invert } from "../../lib/math.js";
 
 const gl = document.querySelector('#gl-canvas').getContext('webgl');
@@ -11,20 +10,20 @@ const programInfo = initProgram(gl);
 const coneBuffer = initConeBuffer(gl);
 const cylinderBuffer = initCylinderBuffer(gl);
 
+// 可以被组合使用
+export const drawFram = (_gl, projectionMat, viewMat) => {
+  draw(_gl, programInfo, { coneBuffer, cylinderBuffer }, projectionMat, viewMat);
+}
 
+
+// 单独使用
 const projectionMat = create();
 const fov = Math.PI / 3;
 const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
 perspective(projectionMat, fov, aspect, 0.1, 100.0);
 
-// 这个矩阵是代表相机在世界坐标中的位置和姿态
 const caremaMat = create();
 const viewMat = create();
 invert(viewMat, caremaMat);
-
-// 可被复用
-export const drawFram = (_gl, projectionMat, viewMat) => {
-  draw(_gl, programInfo, { coneBuffer, cylinderBuffer }, projectionMat, viewMat);
-}
 
 drawFram(gl, projectionMat, viewMat);
--- practices/disco-room/draw.js
+++ practices/disco-room/draw.js
@@ -3,7 +3,24 @@ import { clearCanvasToColor } from '../../lib/utils.js';
 import { drawFram as drawCoord } from '../coord/main.js';
 import { degToRad } from '../../lib/utils.js';
 
-const drawDiscoRoom = (gl, programInfo, buffers, projectionMat, viewMat) => {
+export const draw = (gl, programInfo, buffers, values) => {
+  clearCanvasToColor(gl, [1.0, 1.0, 1.0]);
+
+  const projectionMat = create();
+  const fov = Math.PI / 3;
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  perspective(projectionMat, fov, aspect, 0.1, 100.0);
+
+  let caremaMat = create();
+  caremaMat = m4.yRotate(caremaMat, degToRad(values['cam-rotate-y']))
+  caremaMat = m4.translate(caremaMat, 0, 0, values['cam-trans-z'])
+  const viewMat = create();
+  invert(viewMat, caremaMat);
+
+  // 使用坐标组件
+  drawCoord(gl, projectionMat, viewMat);
+
+  // 绘制当前组件
   gl.useProgram(programInfo.program);
   const { pos, color } = programInfo.attribLocations;
   const { uniformLocations } = programInfo;
@@ -23,23 +40,4 @@ const drawDiscoRoom = (gl, programInfo, buffers, projectionMat, viewMat) => {
   gl.uniformMatrix4fv(uniformLocations.modelMat, false, modelMat);
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indexBuffer);
   gl.drawElements(gl.TRIANGLE_STRIP, buffers.elementNum, gl.UNSIGNED_SHORT, 0);
-}
-
-export const draw = (gl, programInfo, buffers, values) => {
-  clearCanvasToColor(gl, [1.0, 1.0, 1.0]);
-
-  const projectionMat = create();
-  const fov = Math.PI / 3;
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  perspective(projectionMat, fov, aspect, 0.1, 100.0);
-
-  let caremaMat = create();
-  caremaMat = m4.yRotate(caremaMat, degToRad(values['cam-rotate-y']))
-  caremaMat = m4.translate(caremaMat, 0, 0, values['cam-trans-z'])
-  const viewMat = create();
-  invert(viewMat, caremaMat);
-
-  drawDiscoRoom(gl, programInfo, buffers, projectionMat, viewMat)
-
-  drawCoord(gl, projectionMat, viewMat);
 }
