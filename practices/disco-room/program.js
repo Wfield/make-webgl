2bee794f67c5b4c761c49224a76354c6885ef41d
野民 <yemin.lxl@alibaba-inc.com>
2022-06-14 15:03:26 +0800

    fix: update

--- practices/disco-room/program.js
+++ practices/disco-room/program.js
@@ -1,6 +1,7 @@
 import { initShader } from '../../lib/shader.js';
 
 const vertexShader = `
+precision highp float;
 attribute vec4 pos;
 attribute vec4 color;
 
@@ -8,19 +9,35 @@ uniform mat4 modelMat;
 uniform mat4 viewMat;
 uniform mat4 projectionMat;
 
-varying highp vec4 vColor;
+uniform mat4 normalMat;
+
+varying vec4 vColor;
+varying vec3 vLighting;
 
 void main() {
   gl_Position = projectionMat * viewMat *  modelMat * pos;
+
   vColor = color;
+
+  float diffuseStrength = ???
+  float diffuseFactor = max(dot(transformedNormal.xyz, diffuseDir), 0.0);
+  vec3 diffuseColor = ???
+  vec3 diffuse = diffuseColor * diffuseFactor * diffuseStrength;
+
+  float ambientStrength = ???
+  vec3 ambientColor = ???
+  vec3 ambient = ambientColor * ambientStrength;
+  vLinghting = ambient + diffuse;
 }
 `
 
 const fragmentShader = `
-varying highp vec4 vColor;
+varying highp float;
+varying vec4 vColor;
+varying vec3 vLighting;
 
 void main() {
-  gl_FragColor = vColor;
+  gl_FragColor = vec4(vColor.rgb * vLighting, 1.0);
 }
 `
 
